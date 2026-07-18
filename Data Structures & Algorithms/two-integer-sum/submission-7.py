42822cd3540d8cb51730ae237937b7f7c5a158ed
Adil Zhetigenov <101692777+adilzhetigenov@users.noreply.github.com>
2026-07-18 17:59:23 +0200

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+    
+        indices = {nums[i]:[] for i in range(n)}
+        for i in range(n):
+            indices[nums[i]].append(i)
+        nums.sort()
+        l=0
+        r=n-1
+        while l<r:
+            curSum = nums[l] + nums[r]
+            if curSum == target:
+                if nums[l] == nums[r]:
+                    return indices[nums[l]]
+                return sorted([(indices[nums[l]][0]),(indices[nums[r]][0])])
+            elif curSum > target:
+                r -=1
+            else:
+                l +=1
+
